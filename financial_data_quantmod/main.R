2951ccb2dcf29615ffe02ebc7d6205784994584f
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2023-01-16 10:33:19 +0100

    Create financial_data_quantmod

---
+++ financial_data_quantmod/main.R
@@ -0,0 +1,26 @@
+#  Exploring the quantmod package for real-time data
+
+## Load packages
+
+library(doParallel)
+library(quantmod)
+
+## Load parallelization
+
+cl <- makePSOCKcluster(8)
+registerDoParallel(cl)
+
+## Load data
+
+getSymbols("NFLX")
+
+## Standard financial chart
+
+chartSeries(NFLX,
+    type = "auto",
+    line.type = "s"
+)
+
+## Stop Parallelization
+
+stopCluster(cl)
