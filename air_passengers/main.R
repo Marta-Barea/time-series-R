bc015821c6cda23c3279ef584d6d9fb95618b1ba
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2023-01-20 17:24:38 +0100

    Created air_passengers folder

---
+++ air_passengers/main.R
@@ -0,0 +1,113 @@
+#  Time Series Analysis and Modeling with the Air Passengers Dataset
+
+## Load packages
+
+library(doParallel)
+library(forecast)
+library(fpp2)
+
+## Load Parallelization
+
+cl <- makePSOCKcluster(8)
+registerDoParallel(cl)
+
+## Load data
+
+data(AirPassengers)
+ap_data <- AirPassengers
+
+## Inspect data
+
+class(ap_data)
+ap_data
+anyNA(ap_data)
+
+## Descriptive statistics
+
+summary(ap_data)
+
+##  Time plot
+
+p1 <- autoplot(ap_data) +
+    labs(
+        x = "Year",
+        y = "Passengers (1000s)",
+        title = "Number of Passengers from 1949 to 1960"
+    ) +
+    theme(
+        axis.title = element_text(
+            face = "bold"
+        ),
+        title = element_text(
+            face = "bold"
+        )
+    ) +
+    geom_point(color = "purple")
+
+p1
+
+# ggsave("air_passengers/ts_plot.svg")
+
+## Boxplot
+
+boxplot(ap_data ~ cycle(ap_data),
+    xlab = "",
+    ylab = ""
+)
+
+rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4],
+    col = "#ebebeb"
+)
+
+grid(
+    nx = NULL, ny = NULL, col = "white", lty = 1,
+    lwd = par("lwd"), equilogs = TRUE
+)
+
+par(new = TRUE)
+
+# svg("air_passengers/boxplot.svg")
+
+p2 <- boxplot(ap_data ~ cycle(ap_data),
+    xlab = expression(bold("Months")),
+    ylab = expression(bold("Passengers (1000s)")),
+    main = "Monthly Air Passengers Boxplot from 1949 to 1960",
+    col = "purple",
+    whislty = 2,
+)
+
+p2
+
+# dev.off()
+
+## Time serie decomposition: multiplicative classical decompostion
+
+ap_decompose <- decompose(ap_data, type = "multiplicative")
+
+p3 <- autoplot(ap_decompose) +
+    theme(
+        axis.title = element_text(
+            face = "bold"
+        ),
+        title = element_text(
+            face = "bold"
+        ),
+        strip.background = element_rect(
+            fill = "purple"
+        ),
+        strip.text = element_text(
+            color = "white"
+        )
+    ) +
+    labs(
+        x = "Year",
+        y = "Passengers (1000s)"
+    )
+
+p3
+
+# ggsave("air_passengers/ts_decomposition.svg")
+
+## Stop Parallelization
+
+stopCluster(cl)
